32c49bedb9542ef81235b99a85e1609a13dae1fb
지나 <@DESKTOP-ITULMD6.mshome.net>
2016-11-24 18:34:48 +0900

    다시올려요

--- flamans/src/main/java/flamans/controller/EventController.java
+++ flamans/src/main/java/flamans/controller/EventController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import flamans.paging.PageModule;
 import flamans.user.event.model.EventDAO;
 import flamans.user.event.model.EventDTO;
-
 
 @Controller 
 public class EventController {
@@ -41,6 +41,8 @@ public class EventController {
 	@RequestMapping("/event_user_content.do")
 	public ModelAndView event_user_content(@RequestParam(value="e_idx",defaultValue="0")int e_idx){
 		Object list=eventDao.event_user_content(e_idx);
+		int count = eventDao.e_readnum(e_idx);
+		System.out.println(count);
 		ModelAndView mav=new ModelAndView();
 		mav.addObject("list",list);
 		mav.setViewName("event/event_user_content");
@@ -48,12 +50,41 @@ public class EventController {
 	}
 	
 	@RequestMapping("/event_user_endlist.do")
-	public ModelAndView eventUserEndList(){
-			List<EventDTO> list=eventDao.event_user_endlist();
+	public ModelAndView eventUserEndList(
+			@RequestParam(value="cp",defaultValue="1")int cp){
+		int totalCnt=eventDao.getTotalCnt();
+		int listSize=5;
+		int pageSize=5;
+		String pageStr=paging.makePage("event_user_endlist.do",totalCnt,listSize,pageSize,cp);
+			List<EventDTO> list=eventDao.event_user_endlist(cp,listSize);
 			ModelAndView mav=new ModelAndView();
 			mav.addObject("list",list);
+			mav.addObject("pageStr",pageStr);
 			mav.setViewName("event/event_user_endlist");
 			return mav;
 		}
 	
+	@RequestMapping(value="/event_add.do",method=RequestMethod.GET)
+	public ModelAndView event_add(){
+		
+	
+	ModelAndView mav=new ModelAndView();
+	
+	mav.setViewName("event/event_add");
+	
+	return mav;
 }
+	
+	@RequestMapping(value="/event_add.do",method=RequestMethod.POST)
+	public ModelAndView event_add(EventDTO dto){
+		
+	int result=eventDao.event_add(dto);
+	String msg=result>0?"글쓰기 성공":"글쓰기 실패";
+	
+	ModelAndView mav=new ModelAndView();
+	mav.addObject("msg",msg);
+	mav.setViewName("event/eventMsg");
+	return mav;
+}
+   }
+
